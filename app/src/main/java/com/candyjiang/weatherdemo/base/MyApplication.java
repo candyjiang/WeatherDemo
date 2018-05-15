66eac215c05dd424c8fb7c6d2ab26bd3f52901a8
othzjiang <284934531@qq.com>
2018-05-15 11:26:06 +0800

    创建数据库和表配置

---
+++ app/src/main/java/com/candyjiang/weatherdemo/base/MyApplication.java
@@ -0,0 +1,16 @@
+package com.candyjiang.weatherdemo.base;
+
+import android.app.Application;
+
+/**
+ * Created by othzjiang
+ * on 2018/5/15.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/candyjiang/weatherdemo/db/Province.java
@@ -0,0 +1,41 @@
+package com.candyjiang.weatherdemo.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by othzjiang
+ * on 2018/5/15.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
